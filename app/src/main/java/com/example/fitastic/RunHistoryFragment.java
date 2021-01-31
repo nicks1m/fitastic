82a2fe1a76e91f4f21ce5b60644b9466f38edb04
Ethan-67 <ethanrmaguire@hotmail.com>
2021-01-31 16:16:06 +0000

    Linked run page to start on nav

--- app/src/main/java/com/example/fitastic/RunHistoryFragment.java
+++ app/src/main/java/com/example/fitastic/RunHistoryFragment.java
@@ -1,11 +1,17 @@
 package com.example.fitastic;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -54,10 +60,55 @@ public class RunHistoryFragment extends Fragment {
         }
     }
 
+    // method to get sql data (speed distance time)
+    public ArrayList<int[]> getRunStats() {
+        ArrayList<int[]> statistics = new ArrayList<>();
+
+        // generate stats as an example (this should be retrieved from sql)
+        int[] a = {10, 10, 10};
+        int[] b = {5, 5, 5};
+        int[] c = {7, 7, 7};
+
+        statistics.add(a);
+        statistics.add(b);
+        statistics.add(c);
+
+        return statistics;
+    }
+
+    // generate text view
+    @SuppressLint("SetTextI18n")
+    public TextView generateStat(Context context, int i) {
+        TextView stat = new TextView(getContext());
+        ArrayList<int[]> allStats = getRunStats();
+
+        stat.setText("Speed: " + allStats.get(i)[0] + " \t" +
+                "Distance: " + allStats.get(i)[1] + " \t" +
+                "Time: " + allStats.get(i)[2] + " \t");
+
+        stat.setTextSize(18);
+        return stat;
+    }
+
+    // handle creating dynamic elements
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_history_run, container, false);
+
+        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+
+
+        View rootView = inflater.inflate(R.layout.fragment_history_run, container, false);
+
+        LinearLayout myLayout = (LinearLayout)rootView.findViewById(R.id.runHistoryRLayout);
+
+        for (int i = 0; i < getRunStats().size(); i++) {
+            TextView myStat = generateStat(getContext(), i);
+            myLayout.addView(myStat, buttonParams);
+        }
+
+        return rootView;
     }
 }
