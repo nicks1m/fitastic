80632e7b1efd551237e0ba6b227083f926412d97
nicks <simtaokai@hotmail.com>
2021-02-05 22:46:34 +0000

    BMI Calculator Activity and Fragment Page (WIP)

--- app/src/main/java/com/example/fitastic/BMIActivity.java
+++ app/src/main/java/com/example/fitastic/BMIActivity.java
@@ -1,7 +1,10 @@
 package com.example.fitastic;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -22,19 +25,31 @@ public class BMIActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_b_m_i);
 
+        calculateBMI = (Button) findViewById(R.id.calculate_bmi);
+
+        calculateBMI.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //To implement once able to open fragment from bmi activity. Numberformatexception being thrown, resolve it.
+                // Use bundle to send data to next fragment
+
+                //initializeDetails();
+                //userBMI = calculateBMI(getWeight(),getHeight());
+                BMIFrag bmi = new BMIFrag();
+                androidx.fragment.app.FragmentTransaction ft1 = getSupportFragmentManager().beginTransaction();
+                ft1.replace(R.id.bmiFlplaceholder, bmi);
+                ft1.commit();
+            }
+        } );
+    }
+
+    public void initializeDetails(){
         //Get User Weight
         userWeight = Double.parseDouble(String.valueOf(findViewById(R.id.userWeight))) ;
         //Get User Height
         userHeight = Double.parseDouble(String.valueOf(findViewById(R.id.userHeight)));
         setWeight(userWeight);
         setHeight(userHeight);
-
-        calculateBMI.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                userBMI = calculateBMI(getWeight(),getHeight());
-            }
-        } );
     }
 
 
