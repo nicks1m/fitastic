acad56d649c59f934b9cb1b7e4821ad685ecd9d8
Ethan-67 <ethanrmaguire@hotmail.com>
2021-02-06 10:04:04 +0000

    Added initial google maps page to log run

--- app/src/main/java/com/example/fitastic/LogRunFragment.java
+++ app/src/main/java/com/example/fitastic/LogRunFragment.java
@@ -1,100 +1,67 @@
 package com.example.fitastic;
 
-import android.content.Intent;
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
-import java.util.ArrayList;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link LogRunFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class LogRunFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public LogRunFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LogRunFragment.
+    /* Log Run fragment provides users a fragment to map their runs with, this would be an instance
+    of google maps which would need to remember the users start point of the run. Then a line would
+    be drawn throughout the route of the run until the end point. Finally a prompt should be
+    displayed showing stats such as average pace, distance, time ..
      */
-    // TODO: Rename and change types and number of parameters
-    public static LogRunFragment newInstance(String param1, String param2) {
-        LogRunFragment fragment = new LogRunFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    // validate log form
-    public boolean validateForm(int speed, int distance, int time) {
-        // check if input is empty
-
-
-        return true;
-    }
-
-    public boolean isInputEmpty(int x) {
-        return false;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            /* In future Latitudes and Longitudes would need to be retrieved from the users
+            * location to map their run */
+            // create a latitude and longitude object for london's location
+            LatLng london = new LatLng(51.5074, - 0.1278);
+            // add marker to googleMap instance
+            googleMap.addMarker(new MarkerOptions().position(london).title("Marker in London"));
+            // set position of camera to london marker, zoom factor of 20f
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(london, 20f));
         }
-    }
+    };
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_log_run, container, false);
     }
-    // executes after views for fragment has been created
+
     @Override
-    public void onViewCreated(View v, Bundle savedInstanceState) {
-        super.onViewCreated(v, savedInstanceState);
-
-        // add onClick listener to submit button within log form
-        ((Button) v.findViewById(R.id.btnSubmitLog)).setOnClickListener(x -> {
-            int speed = R.id.tvAvgSpeed;
-            int distance = R.id.tvDistance;
-            int time = R.id.tvTime;
-
-            // validate inputs
-            if (validateForm(speed, distance, time)) {
-                // parse data to sql from here
-
-            }
-        });
-
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+        }
     }
 }
