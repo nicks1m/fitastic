377c199eecc4f20d819b5051642336985e9ff8d1
nicks <simtaokai@hotmail.com>
2021-02-20 20:53:08 +0000

    Custom_workout update

--- app/src/main/java/com/example/fitastic/createCustomWorkout.java
+++ app/src/main/java/com/example/fitastic/createCustomWorkout.java
@@ -2,11 +2,21 @@ package com.example.fitastic;
 
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +24,13 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class createCustomWorkout extends Fragment {
+
+    private EditText exercise_title;
+    private EditText exercise_reps;
+    private EditText exercise_set;
+    private Button add_exercise;
+    private Button save_exercise;
+    private List<EditText>custom_workout;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -59,6 +76,66 @@ public class createCustomWorkout extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_create_custom_workout, container, false);
+        View v = inflater.inflate(R.layout.fragment_create_custom_workout, container, false);
+        custom_workout = new ArrayList<>();
+        save_exercise = v.findViewById(R.id.btn_save_exercise);
+        save_exercise.setOnClickListener(v1->{
+            saveExercises();
+        });
+
+        add_exercise = v.findViewById(R.id.btn_add_exercise);
+        add_exercise.setOnClickListener(v1->{
+            addExercise();
+        });
+        return v;
     }
+
+    public void addExercise(){
+//use array list to store EDTs and iterate to get values and push to firebasere
+
+//        LinearLayout layout = getView().findViewById(R.id.exercise_layout);
+
+        LinearLayout layout_box = new LinearLayout(getContext());
+        LinearLayout layout_container = getView().findViewById(R.id.layout_container);
+
+        exercise_title = new EditText(getContext());
+        exercise_title.setText("Title");
+        exercise_title.setEms(7);
+        exercise_title.setTextColor(getResources().getColor(R.color.cyberYellow));
+        exercise_set = new EditText(getContext());
+        exercise_set.setText("Set");
+        exercise_set.setEms(7);
+        exercise_set.setTextColor(getResources().getColor(R.color.cyberYellow));
+        exercise_reps = new EditText(getContext());
+        exercise_reps.setText("Reps");
+        exercise_reps.setEms(3);
+        exercise_reps.setTextColor(getResources().getColor(R.color.cyberYellow));
+
+        //Add to arraylist
+        custom_workout.add(exercise_title);
+        custom_workout.add(exercise_set);
+        custom_workout.add(exercise_reps);
+
+        //Create LinearLayout to store
+        layout_box.addView(exercise_title);
+        layout_box.addView(exercise_set);
+        layout_box.addView(exercise_reps);
+
+        //Add to parent layout container
+        layout_container.addView(layout_box);
+
+
+
+    }
+
+    public void saveExercises(){
+        for(int i = 0; i < custom_workout.size(); i++){
+            System.out.println(custom_workout.get(i).getText().toString());
+        }
+
+        //Create new ArraylIST that stores <Exercise> and push editText into it before firebase???
+
+
+    }
+
 }
