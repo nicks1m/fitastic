24ef3103975c5f4618c4df5d268ecaeb78ddc6e0
nicks <simtaokai@hotmail.com>
2021-02-25 21:37:12 +0000

    Custom_Workout FULLY Functional

--- app/src/main/java/com/example/fitastic/createCustomWorkout.java
+++ app/src/main/java/com/example/fitastic/createCustomWorkout.java
@@ -48,6 +48,7 @@ public class createCustomWorkout extends Fragment {
     private Button start_workout;
     private List<EditText>custom_workout_edt;
     private List<Exercise>custom_workout;
+    private ArrayList<String>list_of_exercises;
     private String custom_workout_title;
     private ValueEventListener mListener;
     private DatabaseReference ref;
@@ -157,12 +158,25 @@ public class createCustomWorkout extends Fragment {
 
         start_workout = v.findViewById(R.id.btn_start_workout);
         start_workout.setOnClickListener(v1->{
+            passArray();
             Bundle args = new Bundle();
+            args.putString("exercises", list_of_exercises.get(list_of_exercises.size() - 4));
+            args.putStringArrayList("array",list_of_exercises);
             args.putString("custom_workout_title",edit_workout_title.getText().toString());
           controller.navigate(R.id.action_createCustomWorkout_to_workout_in_prog_frag, args);
         });
 
         return v;
+    }
+
+    public void passArray(){
+
+        list_of_exercises = new ArrayList<>();
+        for(int i = 0; i <custom_workout_edt.size(); i ++){
+            list_of_exercises.add(custom_workout_edt.get(i).getText().toString());
+        }
+
+        System.out.println(list_of_exercises);
     }
 
     public void removeExercise(String key){
@@ -185,26 +199,30 @@ public class createCustomWorkout extends Fragment {
         LinearLayout layout_box = new LinearLayout(getContext());
 
         //Create the EditTexts for the three fields and add it to the linear layout
+        //exercise_id is the index of exercise in firebase DB. We will use it in future to search for the exercise in the ArrayList.
+        exercise_id = new  EditText(getContext());
+        exercise_id.setText(index);
+        exercise_id.setVisibility(View.INVISIBLE);
+
         exercise_title = new EditText(getContext());
         exercise_title.setText(title);
         exercise_title.setEms(7);
         exercise_title.setTextColor(getResources().getColor(R.color.cyberYellow));
 
         exercise_set = new EditText(getContext());
+        exercise_set.setInputType(2);
         exercise_set.setText(set);
-        exercise_set.setEms(2);
+        exercise_set.setEms(5);
         exercise_set.setTextColor(getResources().getColor(R.color.cyberYellow));
 
         exercise_reps = new EditText(getContext());
+        exercise_reps.setInputType(2);
         exercise_reps.setText(reps);
         exercise_reps.setEms(2);
         exercise_reps.setTextColor(getResources().getColor(R.color.cyberYellow));
 
-        exercise_id = new  EditText(getContext());
-        exercise_id.setText(index);
-        exercise_id.setVisibility(View.INVISIBLE);
-
         Button remove = new Button(getContext());
+        remove.setWidth(10);
         remove.setText("X");
         remove.setEms(2);
         remove.setTextColor(getResources().getColor(R.color.cyberYellow));
@@ -224,10 +242,9 @@ public class createCustomWorkout extends Fragment {
         layout_box.addView(exercise_set);
         layout_box.addView(exercise_reps);
         layout_box.addView(remove);
+
         //Add to parent layout container
         layout_container.addView(layout_box);
-
-
 
     }
 
@@ -238,7 +255,7 @@ public class createCustomWorkout extends Fragment {
             String workout_title = custom_workout_edt.get(i+1).getText().toString();
             String workout_set = custom_workout_edt.get(i+2).getText().toString();
             String workout_reps = custom_workout_edt.get(i+3).getText().toString();
-            Exercise newExercise = new Exercise(workout_title,workout_reps,workout_set);
+            Exercise newExercise = new Exercise(workout_title,workout_set,workout_reps);
             custom_workout.add(newExercise);
             i = i + 3;
         }
