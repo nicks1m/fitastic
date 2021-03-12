daa354ff7c29b1a15d4e44d0bebdd35789e60345
slvQQ <slvandrei123@gmail.com>
2021-03-12 13:53:11 +0000

    friends activity implemented

--- app/src/main/java/com/example/fitastic/findFriends.java
+++ app/src/main/java/com/example/fitastic/findFriends.java
@@ -1,21 +1,30 @@
 package com.example.fitastic;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
 
 public class findFriends extends AppCompatActivity {
 
-    TextView friend_text;
+    private Toolbar mToolbar;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_friends);
 
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//        getSupportActionBar().setTitle("Update Post");
+
+
     }
-
-
-
 
 }
