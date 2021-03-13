bb4586f96340ec7effc78584d8237995f0f3e239
nicks <simtaokai@hotmail.com>
2021-03-13 14:13:01 +0000

    Rewards Page implementation WIP

--- app/src/main/java/com/example/fitastic/rewardsFrag.java
+++ app/src/main/java/com/example/fitastic/rewardsFrag.java
@@ -8,6 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +19,11 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +34,19 @@ public class rewardsFrag extends Fragment {
 
     private FirebaseAuth auth;
     private DatabaseReference mDatabase;
+    private LinearLayout avail_rewards;
+    private ArrayList<Integer> tierLevels;
+    private LinearLayout claim_history;
+    private LinearLayout reward_box;
+    private LinearLayout claim_box;
+    private TextView brand;
+    private TextView type;
+    private TextView discount;
+    private TextView points;
+    private Button claim;
+    private String spendingPoints;
+    private TextView spending_pts;
+    private TextView tierLevel;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -73,17 +95,49 @@ public class rewardsFrag extends Fragment {
 //        rewardsGenerator.
 
         View v = inflater.inflate(R.layout.fragment_rewards, container, false);
+        avail_rewards = v.findViewById(R.id.available_rewards);
+        spending_pts = v.findViewById(R.id.spending_pts);
+        tierLevel = v.findViewById(R.id.tier_level);
+        claim_history = v.findViewById(R.id.claim_history);
+
+
+        initializeTiers();
 
         auth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference ref = mDatabase.child("Users").child(auth.getCurrentUser().getUid()).child("points").child("unclaimed");
+        DatabaseReference ref = mDatabase.child("Users").child(auth.getCurrentUser().getUid()).child("points");
+
+
         ref.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                //while loop to check, generate rewards accordingly
-                if(snapshot.getChildrenCount() < 5){
-                    Object a = new rewardsGenerator(1);
-                    ref.push().setValue(a);
+                removeViews();
+                int tierPoints = Integer.parseInt(snapshot.child("tierpoints").getValue().toString());
+                spendingPoints = snapshot.child("availpoints").getValue().toString();
+                spending_pts.setText(spendingPoints);
+                tierLevel.setText("Tier " + calculateTier(tierPoints) + " Rewards");
+
+                //if rewards < 5 in list, update with new rewards
+                if(snapshot.child("unclaimed").getChildrenCount() < 5){
+                    Object a = new rewardsGenerator(calculateTier(tierPoints));
+                    ref.child("unclaimed").push().setValue(a);
+                }
+                //generate views
+                for(DataSnapshot r : snapshot.child("unclaimed").getChildren()){
+//                    DataSnapshot r = snapshot.child("unclaimed");
+                    addRewards(r.getKey(),
+                            r.child("brands").getValue().toString(),
+                            r.child("type").getValue().toString(),
+                            r.child("discount").getValue().toString(),
+                            r.child("points").getValue().toString());
+                }
+
+                for(DataSnapshot r : snapshot.child("claimed").getChildren()){
+                    addClaimedRewards(r.getKey(),
+                            r.child("brands").getValue().toString(),
+                            r.child("type").getValue().toString(),
+                            r.child("discount").getValue().toString(),
+                            r.child("points").getValue().toString());
                 }
 
 
@@ -96,16 +150,157 @@ public class rewardsFrag extends Fragment {
         });
 
         //generate, Push data to firebase, check if the amount of rewards is less < 5. if true, push new reward to fill missing slots
-        rewardsGenerator.generate(1);
-        for(int i = 0; i < rewardsGenerator.rewards.size(); i++){
-            System.out.println(
-                    "Brand: " + rewardsGenerator.rewards.get(i).getBrands() +
-                    "Type: " + rewardsGenerator.rewards.get(i).getType() +
-                    "Discount: " + rewardsGenerator.rewards.get(i).getDiscount() +
-                    "Points Required: " + rewardsGenerator.rewards.get(i).getPoints());
-
-        }
+//        rewardsGenerator.generate(1);
+//        for(int i = 0; i < rewardsGenerator.rewards.size(); i++){
+//            System.out.println(
+//                    "Brand: " + rewardsGenerator.rewards.get(i).getBrands() +
+//                    "Type: " + rewardsGenerator.rewards.get(i).getType() +
+//                    "Discount: " + rewardsGenerator.rewards.get(i).getDiscount() +
+//                    "Points Required: " + rewardsGenerator.rewards.get(i).getPoints());
+//
+//        }
 
         return v;
     }
-}
+    private void addClaimedRewards(String id, String brand_name, String type_item, String discount_amt, String points_req) {
+        String key = id;
+
+        claim_box = new LinearLayout(getContext());
+        claim_box.setMinimumHeight(100);
+        claim_box.setBackgroundColor(getResources().getColor(R.color.cardGrey));
+
+        brand = new TextView(getContext());
+        brand.setText(brand_name);
+        brand.setPadding(50,0,0,0);
+        brand.setEms(10);
+
+        type = new TextView(getContext());
+        type.setText(type_item);
+        type.setEms(5);
+
+
+        discount = new TextView(getContext());
+        discount.setText(discount_amt + "%");
+        discount.setEms(3);
+
+
+        points = new TextView(getContext());
+        points.setText(points_req);
+        points.setEms(4);
+
+        claim_box.addView(brand);
+        claim_box.addView(type);
+        claim_box.addView(discount);
+        claim_box.addView(points);
+
+
+        claim_history.addView(claim_box);
+    }
+
+    private void addRewards(String id, String brand_name, String type_item, String discount_amt, String points_req){
+
+        String key = id;
+
+        reward_box = new LinearLayout(getContext());
+        reward_box.setMinimumHeight(100);
+
+        brand = new TextView(getContext());
+        brand.setText(brand_name);
+        brand.setPadding(50,0,0,0);
+        brand.setEms(8);
+
+        type = new TextView(getContext());
+        type.setText(type_item);
+        type.setEms(5);
+
+
+        discount = new TextView(getContext());
+        discount.setText(discount_amt + "%");
+        discount.setEms(3);
+
+
+        points = new TextView(getContext());
+        points.setText(points_req);
+        points.setEms(3);
+
+        claim = new Button(getContext());
+        claim.setText("claim");
+        claim.setTextSize(12);
+        claim.setBackgroundColor(getResources().getColor(R.color.pageBG));
+        claim.setOnClickListener(v->{
+            DatabaseReference ref = mDatabase.child("Users").child(auth.getCurrentUser().getUid()).child("points");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    //calculate new amt of points
+                    if(Integer.parseInt(snapshot.child("availpoints").getValue().toString()) < Integer.parseInt(points_req)){
+                        Toast.makeText(getContext(), "Not enough Points", Toast.LENGTH_SHORT).show();
+                        return;
+
+                    } else {
+                        int newPoints = Integer.parseInt(snapshot.child("availpoints").getValue().toString()) - Integer.parseInt(points_req);
+                        if(newPoints < 0){
+                            newPoints = 0;
+                        }
+                        ref.child("availpoints").setValue(String.valueOf(newPoints));
+
+                        //create hashmap to store details
+                        HashMap<String, Object> reward = new HashMap<>();
+                        reward.put("brands", brand_name);
+                        reward.put("type", type_item);
+                        reward.put("discount", discount_amt);
+                        reward.put("points", points_req);
+                        //move data to claimed history
+                        ref.child("claimed").push().setValue(reward);
+
+                        reward_box.setVisibility(View.GONE);
+
+                        ref.child("unclaimed").child(key).removeValue();
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+            });
+        });
+
+        reward_box.addView(brand);
+        reward_box.addView(type);
+        reward_box.addView(discount);
+        reward_box.addView(points);
+        reward_box.addView(claim);
+
+        avail_rewards.addView(reward_box);
+
+    }
+
+    private void initializeTiers(){
+
+        tierLevels = new ArrayList<>();
+        for(int i = 0; i < 10; i ++){
+            tierLevels.add((i+1)*1500);
+
+        }
+    }
+
+    private int calculateTier(int points){
+        int tier = 0;
+        for(int i = 0; i <tierLevels.size(); i ++){
+            if(points < tierLevels.get(i)) {
+                System.out.println("current tier: " + i);
+                tier = i;
+                return tier;
+            }
+
+            }
+        return tier;
+    }
+
+
+    private void removeViews(){
+        avail_rewards.removeAllViews();
+        claim_history.removeAllViews();
+    }
+    }
